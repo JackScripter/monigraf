b56e127ca2715eb0eb2c6443da721dd929867d59
Jacob Ouellette <jackscripter45@gmail.com>
2020-06-10 13:09:38 -0400

    Fix snmp environment sensor value to int. Checkpoint OID

--- SNMP/OID.py
+++ SNMP/OID.py
@@ -2,6 +2,7 @@
 
 class oid:
     sysinfo = "1.3.6.1.2.1.1.1.0"
+    sysinfo_cp = "1.3.6.1.4.1.2620.1.6.5.1.0"
     class CiscoCatalyst:
         # System
         cpu_usage = "1.3.6.1.4.1.9.9.109.1.1.1.1.3"
@@ -21,6 +22,17 @@ class oid:
         # Environment Sensor
 
         # Interface
+        if_desc = "1.3.6.1.2.1.2.2.1.2"
+        total_in_octets = "1.3.6.1.2.1.2.2.1.10"
+        total_out_octets = "1.3.6.1.2.1.2.2.1.16"
+
+    class CheckPoint:
+        sysinfo = "1.3.6.1.4.1.2620.1.6.5.1.0"
+        cpu_usage = "1.3.6.1.4.1.2620.1.6.7.2.4"
+        env_temp_status_desc = "1.3.6.1.4.1.2620.1.6.7.8.1.1.2"
+        env_temp_status_value = "1.3.6.1.4.1.2620.1.6.7.8.1.1.3"
+        env_fan_desc = "1.3.6.1.4.1.2620.1.6.7.8.2.1.2"
+        env_fan_value = "1.3.6.1.4.1.2620.1.6.7.8.2.1.3"
         if_desc = "1.3.6.1.2.1.2.2.1.2"
         total_in_octets = "1.3.6.1.2.1.2.2.1.10"
         total_out_octets = "1.3.6.1.2.1.2.2.1.16"
