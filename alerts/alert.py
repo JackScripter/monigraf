7ac82686d89cfbc2e904ee47da306b4f555e3d3f
JackScripter <jackscripter45@gmail.com>
2020-07-16 18:12:51 -0400

    Dynamically import module

--- alerts/alert.py
+++ alerts/alert.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 import configparser
+import ast
+import importlib
 
 config = configparser.ConfigParser()
 config.read('/etc/monigraf/monigraf.ini')
 
-def GetEnabledAlerts():
-    return config['DEFAULT']['ALERTING_SERVICES']
+class Alerts():
+    def __init__(self, msg):
+        for item in ast.literal_eval(config['DEFAULT']['ALERTING_SERVICES']):
+            full_module = item + '.' + item
+            module = importlib.import_module(full_module)
+            module.SendText(msg)
