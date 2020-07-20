11f3569158ff4265170094f9514df0ac4a3c6467
JackScripter <jackscripter45@gmail.com>
2020-07-20 19:16:30 -0400

    Print timestamp in alert

--- alerts/alert.py
+++ alerts/alert.py
@@ -2,6 +2,7 @@
 import configparser
 import ast
 import importlib
+import datetime
 
 config = configparser.ConfigParser()
 config.read('/etc/monigraf/monigraf.ini')
@@ -11,4 +12,6 @@ class Alerts():
         for item in ast.literal_eval(config['DEFAULT']['ALERTING_SERVICES']):
             full_module = item + '.' + item
             module = importlib.import_module(full_module)
+            now = str(datetime.datetime.fromtimestamp(datetime.datetime.timestamp(datetime.datetime.now())))
+            msg = str('`[' + now + '] ' + msg + '`')
             module.SendText(msg)
